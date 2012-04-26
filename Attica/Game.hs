50eeb50314940316300d9546479ce676a260ad05
Joseph Tilley <jtilley@gmail.com>
2012-04-26 11:03:56 -0400

    Added combat actions to Game Monad

--- Attica/Game.hs
+++ Attica/Game.hs
@@ -9,6 +9,7 @@ module Attica.Game
 where
 
 import Control.Monad.State
+import Attica.Core
 import Attica.Player
 import Attica.Monster
 
@@ -26,3 +27,30 @@ thePlayer g = do
 	(GameState p _) <- get
 	return p
 
+setPlayer :: Game a -> Player -> Game ()
+setPlayer g p = do
+	(GameState _ m) <- get
+	put $ GameState p m
+	return ()
+
+damagePlayer :: Game a -> Int -> Game ()
+damagePlayer g amt = do 
+	p <- thePlayer g
+	setPlayer g $ damage p amt
+
+setMonster :: Game a -> Monster -> Game ()
+setMonster g m = do
+	(GameState p _) <- get
+	put $ GameState p $ Just m
+
+getMonster :: Game a -> Game (Maybe Monster)
+getMonster g = do
+	(GameState _ m) <- get
+	return m
+
+damageMonster :: Game a -> Int -> Game ()
+damageMonster g amt = do
+	mm <- getMonster g
+	case mm of
+		Just m -> setMonster g $ damage m amt
+		Nothing -> return ()
